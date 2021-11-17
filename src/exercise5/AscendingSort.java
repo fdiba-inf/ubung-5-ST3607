b3bbe51b35de7c17503a7b0f293f2d057950bd89
ST3607 <>
2021-11-17 18:45:54 +0000

    Files

--- src/exercise5/AscendingSort.java
+++ src/exercise5/AscendingSort.java
@@ -16,10 +16,23 @@ public class AscendingSort {
         for (int index = 0; index < numbers.length; index++) {
             numbers[index] = input.nextInt();
         }
+        for (int index = 0; index < numbers.length -1; index++) {
+          int minIndex = index;
 
+          for (int i = index + 1; i < numbers.length; i++) {
+            if (numbers[minIndex] > numbers[i]) {
+              minIndex = i;
+            }
+          }
+          int t = numbers[index];
+          numbers[index] = numbers[minIndex];
+          numbers[minIndex] = t;
+        }
         // Sort numbers in ascending order
+
 
         String numbersAsString = Arrays.toString(numbers);
         System.out.println("Sorted numbers: " + numbersAsString);
     }
 }
+
